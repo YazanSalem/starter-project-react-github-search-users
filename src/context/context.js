cbb26b5740cd1e2d499fcf6ba5dd84e036fa9d52
YazanSalem <yazansalem064@gmail.com>
2022-08-22 20:56:04 -0500

    FusionCharts Components

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,22 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext();
+
+//GithubContext has Provider, Consumer
+
+//Returns Consumer
+const GithubProvider = ({children}) => {
+    const [githubUser, setGithubUser] = useState(mockUser);
+    const [repos, setGithubRepos] = useState(mockRepos);
+    const [followers, setGithubFollowers] = useState(mockFollowers);
+
+    return (
+    <GithubContext.Provider value={{githubUser, repos, followers}}>
+        {children}
+    </GithubContext.Provider>
+    );
+};
+
+export {GithubProvider, GithubContext}
